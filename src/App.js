0e4d85df0521f45b17b2586b243e8ff70395c1bd
Jacob Zuo <chopinsky@live.com>
2018-11-27 23:32:22 -0600

    adding logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import icon from "./logo.png";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Input from "@material-ui/core/Input";
+import bcrypt from "bcrypt";
 
 const styles = {
   card: {
@@ -20,13 +22,19 @@ const styles = {
   },
   pos: {
     marginBottom: 12
+  },
+  icon: {
+    display: "block",
+    margin: "auto",
+    height: "150px",
+    width: "150px"
   }
 };
 
 class App extends Component {
   state = {
     password: "",
-    transmog: "[Enter your password to start]",
+    hash: "[Enter your password to start]",
     entered: false
   };
 
@@ -42,10 +50,15 @@ class App extends Component {
     }
 
     let password = event.target.value;
-    this.setState({
-      password,
-      transmog: btoa(password),
-      entered: true
+
+    bcrypt.hash(password, 5, (err, hash) => {
+      if (!err) {
+        this.setState({
+          password,
+          hash,
+          entered: true
+        });
+      }
     });
   };
 
@@ -53,13 +66,14 @@ class App extends Component {
     if (this.state.entered && this.textInput) {
       this.textInput.select();
       document.execCommand("copy");
-      alert(`Text copied: ${this.state.transmog}`);
+      alert(`Text copied: ${this.state.hash}`);
     }
   };
 
   render() {
     return (
       <div>
+        <img src={icon} style={styles.icon} alt="icon" />
         <Card className={styles.card}>
           <CardContent>
             <Typography
@@ -78,12 +92,12 @@ class App extends Component {
           </CardContent>
           <CardActions>
             <Button size="medium" onClick={this.handleButtonClick}>
-              {this.state.transmog}
+              {this.state.hash}
             </Button>
             <input
               type="text"
               ref={this.setTextInputRef}
-              value={this.state.transmog}
+              value={this.state.hash}
               readOnly={true}
               onClick={this.handleButtonClick}
             />

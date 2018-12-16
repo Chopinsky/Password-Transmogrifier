31e6456d7f4467fd89c1b5742f927d23f23fff4e
Jacob Zuo <chopinsky@live.com>
2018-12-15 18:29:17 -0600

    more

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -6,17 +6,27 @@
       </div>
       <div class="row fieldRow">
         <div class="input-field col s9">
-          <input id="hint" type="text" class="validate" v-model="input" v-on:keyup="onInput">
+          <input id="hint" type="text" class="validate" v-model="input" v-on:keyup="onInputChanged">
           <label for="hint">Hint Phrase</label>
         </div>
         <div class="col s3">
-          <a class="waves-effect waves-teal btn-flat button">Clear</a>
+          <a class="waves-effect waves-teal btn-flat button">
+            <i class="material-icons left icon">cancel</i>Clear
+          </a>
         </div>
       </div>
       <div class="row fieldRow">
         <div class="input-field col s12">
-          <textarea id="password" class="materialize-textarea"/>
-          <label for="password">Generated Password</label>
+          <textarea
+            id="password"
+            class="materialize-textarea"
+            readonly="readOnly"
+            ref="textArea"
+            v-model="password"
+            v-on:focus="onTextAreaFocus"
+            v-on:blur="onTextAreaBlur"
+          />
+          <label for="password" v-bind:class="isActive">Generated Password</label>
         </div>
       </div>
       <div class="row">
@@ -32,12 +42,39 @@ export default {
   name: "app",
   data: () => {
     return {
-      input: ""
+      input: "",
+      password: ""
     };
   },
   methods: {
-    onInput(msg, event) {
-      console.log("input...", this.input);
+    onInputChanged() {
+      if (this.input) {
+        this.password = this.input;
+      } else {
+        this.password = "";
+      }
+    },
+    onTextAreaFocus() {
+      this.$refs["textArea"].select();
+    },
+    onTextAreaBlur() {
+      if (window.getSelection) {
+        if (window.getSelection().empty) {
+          // Chrome
+          window.getSelection().empty();
+        } else if (window.getSelection().removeAllRanges) {
+          // Firefox
+          window.getSelection().removeAllRanges();
+        }
+      } else if (document.selection) {
+        // IE?
+        document.selection.empty();
+      }
+    }
+  },
+  computed: {
+    isActive() {
+      return this.input && this.input.length > 0 ? "active" : "";
     }
   }
 };
@@ -51,21 +88,28 @@ body {
   text-align: center;
   color: #2c3e50;
   background-image: linear-gradient(to right, #fff176, #f9a825);
+  min-width: 600px;
+  overflow: visible;
 }
 .card-custom {
   margin: 10px auto;
   width: 100%;
   max-width: 512px;
+  min-width: 448px;
 }
 .fieldRow {
   padding: 0 20px;
 }
 .button {
   margin-top: 18px;
-  width: 80px;
+  width: 90px;
   height: 40px;
-  padding-top: 0;
-  padding-bottom: 4px;
+  padding: 0 4px 5px 7px;
   border: #f9a825 solid 2px;
+}
+i.icon {
+  margin: 0;
+  padding: 0;
+  width: 20px;
 }
 </style>

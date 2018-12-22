233007064f5a1b0fa4fabec570525bf17d4c3820
Jacob Zuo <chopinsky@live.com>
2018-12-22 16:04:06 -0600

    adding more feature

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -4,11 +4,12 @@
       <div class="row title-row">
         <span class="card-title grey-text text-darken-2">Pass . Made . Easy</span>
       </div>
-      <div class="row fieldRow">
+      <div class="row field-row">
         <div class="input-field col s11">
           <input
             id="hint"
             type="text"
+            ref="input"
             class="validate"
             v-model="input"
             v-on:keydown="onInputChanged"
@@ -25,7 +26,13 @@
           </a>
         </div>
       </div>
-      <div class="row fieldRow">
+      <div class="row inline-row">
+        <label class="checkbox">
+          <input type="checkbox" v-bind:checked="this.checked" v-on:click="onCheckboxClicked">
+          <span>Save this Hint Phrase</span>
+        </label>
+      </div>
+      <div class="row field-row">
         <div class="input-field col s12">
           <textarea
             id="password"
@@ -40,7 +47,7 @@
           <label for="password" v-bind:class="isActive">Generated Password</label>
         </div>
       </div>
-      <div class="row button-row">
+      <div class="row control-row">
         <a
           class="waves-effect waves-light btn"
           title="Copy the generated password to the clipboard"
@@ -59,13 +66,28 @@
 /* eslint-disable */
 import crypto from "crypto";
 
+const INPUT_KEY = "hintPhrase";
+const TO_SAVE_KEY = "savePassPhrase";
+
 export default {
   name: "app",
-  data: () => {
-    //todo: load from chrome saved
+  data: function() {
+    if (chrome && chrome.storage) {
+      input = chrome.storage.sync.get([INPUT_KEY, TO_SAVE_KEY], result => {
+        if (result[TO_SAVE_KEY]) {
+          this.input = result[INPUT_KEY];
+          this.checked = result[TO_SAVE_KEY];
+          this.setPassword(this.hash(result[INPUT_KEY]));
+
+          this.$refs["input"].focus();
+        }
+      });
+    }
+
     return {
       input: "",
-      password: ""
+      password: "",
+      checked: false
     };
   },
   methods: {
@@ -82,7 +104,6 @@ export default {
         }
       }
 
-      console.log(ans);
       return ans;
     },
     setPassword(password) {
@@ -112,6 +133,11 @@ export default {
         document.selection.empty();
       }
     },
+    store(key, value) {
+      if (chrome && chrome.storage) {
+        chrome.storage.sync.set({ key: value });
+      }
+    },
     onInputChanged() {
       this.$nextTick(() => {
         // get input instantly
@@ -125,6 +151,15 @@ export default {
     onButtonClick() {
       this.input = "";
       this.setPassword("");
+    },
+    onCheckboxClicked(event) {
+      if (chrome) {
+        if (this.checked && this.input) {
+          this.store(TO_SAVE_KEY, this.input);
+        } else {
+          this.store(TO_SAVE_KEY, "");
+        }
+      }
     },
     onCopyClick() {
       this.selectCopy();
@@ -164,17 +199,22 @@ div.row {
 div.title-row {
   padding: 20px 0 10px 0;
 }
-div.button-row {
-  padding: 5px;
+div.field-row {
+  padding: 0 20px;
+}
+div.inline-row {
+  margin-bottom: 15px;
+  padding: 0 32px;
+  text-align: left;
+}
+div.control-row {
+  padding: 5px 0;
 }
 .card-custom {
   margin: 15px auto;
   width: 480px;
   max-width: 512px;
   min-width: 448px;
-}
-.fieldRow {
-  padding: 0 20px;
 }
 .button {
   margin-top: 24px;
